7ff9cd2c3e028df42ad4c8a87f46608daead85f1
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-12-23 22:51:58 +0100

    Control the delay, and test with 20 particles

--- slam/test_grid_map.py
+++ slam/test_grid_map.py
@@ -13,12 +13,13 @@ import math
 INF = 500
 
 hw = Hardware("../test/testdata-film-feli01.txt")
-state = State(n_particles=1, cellsize=5, blocksize=100)
+state = State(n_particles=20, cellsize=5, blocksize=100)
 
 old_pose = Pose(0, 0, 0)
 
 i = 0
 ogm = None
+sumdeltas = 0
 for update in hw.updates():
     start_time = time.time()
     state.update(update)
@@ -32,14 +33,15 @@ for update in hw.updates():
     cell = -INF
     old_pose = pose
     i += 1
-    if i % 25 == 0:
-        print(ogm)
-        print(ogm.distance_to_closest_object_in_cone(pose, 0.872664626, 130))
+    #if i % 25 == 0:
+    #    print(ogm)
+    #    print(ogm.distance_to_closest_object_in_cone(pose, 0.872664626, 130))
     stop_time = time.time()
-    timedelta = (stop_time - start_time) * 1000
-    if timedelta > update.timedelta:
-        print("Slower than updates: %f" % (update.timedelta - timedelta))
+    timedelta = update.timedelta - (stop_time - start_time) * 1000
+    sumdeltas += timedelta
+    if timedelta > 0:
+        print("Slower than updates: %f, current delay %f" % (timedelta, sumdeltas))
     else:
-        print("Faster than updates: %f" % (update.timedelta - timedelta))
-
+        print("Faster than updates: %f, current delay %f" % (timedelta, sumdeltas))
+print(sumdeltas)
 print(ogm)

219cdc319fe605068f06e39e94540e9c3ca374c5
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-12-23 13:45:03 +0100

    Add time measurements to check if the algorithm can work as an online algo

--- slam/test_grid_map.py
+++ slam/test_grid_map.py
@@ -13,14 +13,15 @@ import math
 
 INF = 500
 
-hw    = Hardware("../test/controlled_run.txt")
-state = State(cellsize=5, blocksize=100)
+hw    = Hardware("../test/testdata-film-feli01.txt")
+state = State(n_particles=1,cellsize=5, blocksize=100)
 
 old_pose = Pose(0,0,0)
 
 i = 0
 ogm = None
 for update in hw.updates():
+	start_time = time.time()
 	state.update(update)
 	particle = state.particles[0]
 	ogm = particle.map
@@ -35,6 +36,11 @@ for update in hw.updates():
 	if i%25 == 0:
 	    print(ogm)
 	    print(ogm.distance_to_closest_object_in_cone(pose, 0.872664626, 130))
-
+	stop_time = time.time()
+	timedelta = (stop_time - start_time) * 1000
+	if timedelta > update.timedelta:
+		print("Slower than updates: %f" % (update.timedelta - timedelta))
+	else:
+		print("Faster than updates: %f" % (update.timedelta - timedelta))
 
 print(ogm)
